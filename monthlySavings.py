244d44022cb4918895cacf0ea91374105c5f0757
Bruno Figueiredo <21010857+bmalbusca@users.noreply.github.com>
2023-10-28 22:49:38 +0100

    Edited code to functions

--- monthlySavings.py
+++ monthlySavings.py
@@ -1,4 +1,4 @@
-import os
+import os, types
 import pandas
 import tabula
 
@@ -28,9 +28,11 @@ def getLocalFiles(debug = False) -> list:
             existing_files_list.append(filename)
     if debug:
         print(existing_files_list)
+    return {cwd : existing_files_list}
 
-getLocalFiles()
 
+test =getLocalFiles()
+print(test)
 
 def collectCacheData(cache_name='cached_dataframe.pkl',debug=False):
     # Read your DataFrame
@@ -39,69 +41,94 @@ def collectCacheData(cache_name='cached_dataframe.pkl',debug=False):
         print("read_cache", read_cache)
 
     if not read_cache.empty :
-        df = read_cache
-    else:
-        tables = tabula.read_pdf("~/Downloads/extracto.pdf", pages="all")
-        #print(tables[0], type(tables[0]), len(tables[0]))
+        return read_cache
+    return None
 
-        filtered_tables=[]
-        for table in tables:
-            if {'Mov', 'Valor', 'Saldo'}.issubset(table.columns):
+def readPDF(file_dir="~/Downloads/extracto.pdf", debug=False, filter_set_column={'Mov', 'Valor', 'Saldo'}, store_incache=True):
+
+    tables = tabula.read_pdf(file_dir, pages="all")
+    #print(tables[0], type(tables[0]), len(tables[0]))
+
+    filtered_tables=[]
+    for table in tables:
+        if filter_set_column.issubset(table.columns):
                 filtered_tables.append(table)
 
-        df=filtered_tables[0].copy()
+    df=filtered_tables[0].copy()
+    if debug:
         print(len(filtered_tables[0]), len(filtered_tables[1]),len(filtered_tables[0])+ len(filtered_tables[1]) )
     
-        for i in range(len(filtered_tables)-1):
+    for i in range(len(filtered_tables)-1):
+        if debug:
             print("table[",i,"]")
-            if list(filtered_tables[0].columns) == list(filtered_tables[i+1].columns):
-                df=pandas.concat([df, filtered_tables[i+1]], ignore_index = True)
-
+        if list(filtered_tables[0].columns) == list(filtered_tables[i+1].columns):
+            df=pandas.concat([df, filtered_tables[i+1]], ignore_index = True)
+    if debug:
         print(len(filtered_tables[0]),len(df))
         print(df)
     
-        # Store your DataFrame
+    # Store your DataFrame
+    if store_incache:
         df.to_pickle('cached_dataframe.pkl') # will be stored in current directory
     
     return df
 
-df = collectCacheData(debug=True)
-#print("look is NaN")
-#print(df['Saldo'].isnull().values.any()) #There is nan
-#print(list(df.loc[pandas.isna(df["Saldo"]), :].index)) #return the indexes
-#print(df.loc[pandas.isna(df["Saldo"]), :]) # return the rows with nan
+df=collectCacheData(debug=True)
+if isinstance(df, type(None)) == True:
+    df=readPDF()
 
-df=df.dropna(subset=['Saldo']).reset_index(drop=True)
-df['Saldo']=df['Saldo'].apply(str2num)
+def existsNaNDataframe(df, column='Saldo'):
+    print("Nan Values:",df[column].isnull().values.any()) #There is nan
+    print("Nan Values Indexes:",list(df.loc[pandas.isna(df[column]), :].index)) #return the indexes
+    print(df.loc[pandas.isna(df[column]), :]) # return the rows with nan
+
+def parseDataframe(df):
+    df=df.dropna(subset=['Saldo']).reset_index(drop=True)
+    df['Saldo']=df['Saldo'].apply(str2num)
+    df= df.dropna(subset=['Valor.1']).reset_index(drop=True)
+    df['Valor.1'] =df['Valor.1'].apply(str2num)
+    return df
 
 #print(valor.to_string())
-
-value0= df['Saldo'][0]
-valuet= df['Saldo'].iloc[-1]
-diffvalue=float(valuet)-float(value0)
-print("Inicio:", df['Valor'][1],value0,"€   Fim: ", df['Valor'].iloc[-1], valuet, "€  Resultado:", diffvalue,"€")
+df=parseDataframe(df)
+def balanceAmount(df, column='Saldo'):
+    value0= df[column][0]
+    valuet= df[column].iloc[-1]
+    diffvalue=float(valuet)-float(value0)
+    print("Inicio:", df['Valor'][1],value0,"€   Fim: ", df['Valor'].iloc[-1], valuet, "€  Resultado:", diffvalue,"€")
 #print(df.dtypes)
 
-df= df.dropna(subset=['Valor.1']).reset_index(drop=True)
-df['Valor.1'] =df['Valor.1'].apply(str2num)
+balanceAmount(df)
 
-print("======= 10 Maiores Despesas ========")
-print(df.nsmallest(10,'Valor.1', keep='all')[['Mov','Descritivo do Movimento', 'Valor.1']])
-print(" ")
+#df= df.dropna(subset=['Valor.1']).reset_index(drop=True)
+#df['Valor.1'] =df['Valor.1'].apply(str2num)
 
-print("======= Compras acima de 10 euros ========")
-print(df[df['Valor.1']<-10][['Mov','Descritivo do Movimento', 'Valor.1']])
-print(" ")
+#print(df)
 
-print("======= 10 Maiores Recebedores =======")
-receivers= df.groupby("Descritivo do Movimento")["Valor.1"].sum()
-print(receivers.nsmallest(10).reset_index())
-print(" ")
+def expensesBiggest(df,threshold=10):
+    print("======= 10 Maiores Despesas ========")
+    print(df.nsmallest(threshold,'Valor.1', keep='all')[['Mov','Descritivo do Movimento', 'Valor.1']])
+    print(" ")
 
-print("======= 10 Despesas Mais Recorrentes =======")
-recurring_exp = df[(df['Valor.1'] < 0)].groupby("Descritivo do Movimento").size().to_frame(name = 'size').reset_index().sort_values(by='size', ascending=False)
+def expensesAbove(df,threshold=10):
+    print("======= Compras acima de 10 euros ========")
+    print(df[df['Valor.1']<-threshold][['Mov','Descritivo do Movimento', 'Valor.1']])
+    print(" ")
+
+def expensesReceiver(df,threshold=10):
+    print("======= 10 Maiores Recebedores =======")
+    receivers= df.groupby("Descritivo do Movimento")["Valor.1"].sum()
+    print(receivers.nsmallest(threshold).reset_index())
+    print(" ")
+
+def expensesRecurring(df,threshold=10):
+    print("======= 10 Despesas Mais Recorrentes =======")
+    recurring_exp = df[(df['Valor.1'] < 0)].groupby("Descritivo do Movimento").size().to_frame(name = 'size').reset_index().sort_values(by='size', ascending=False)
 #print(recurring_exp.head(10))
-rec_merge = pandas.merge( receivers, recurring_exp, on="Descritivo do Movimento", how='inner') 
-print(rec_merge.sort_values(by='size', ascending=False).rename(columns = {'Valor.1':'Soma', 'size':'Ocurrencias'}).head(10))
+    rec_merge = pandas.merge( receivers, recurring_exp, on="Descritivo do Movimento", how='inner') 
+    print(rec_merge.sort_values(by='size', ascending=False).rename(columns = {'Valor.1':'Soma', 'size':'Ocurrencias'}).head(threshold))
 
+
+expensesAbove(df,30)
+expensesBiggest(df)
 ## Falta fazer menu, guardar por data, fazer grafico 
