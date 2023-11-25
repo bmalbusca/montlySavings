55603061729ce2a22d6e22b2cc48cfe430eb7d87
Bruno Figueiredo <21010857+bmalbusca@users.noreply.github.com>
2023-11-25 18:16:21 +0000

    Add comm line arguments to env script; Create env if dir does not exist

--- start_env.sh
+++ start_env.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
-echo "Activating python enviroment"
-source .msenv/bin/activate
+# Script: start_env.sh
+# Author: bmalbusca
+# Year: 2023
+# Description: Set up and activate a Python virtual environment.
+
+# Default values
+CMD=$1
+DIR=".msenv"
+
+# Check if a command-line argument is provided to set the environment name
+if [[ -n "$CMD" ]]; then
+    DIR=$CMD
+    echo $DIR
+fi
+
+# Set the path to the virtual environment
+PWD=$(pwd)/$DIR
+echo "Activating python environment."
+
+# Check if the environment directory already exists
+if [ -d "$DIR" ]; then
+    echo "The $DIR exists."
+else
+    # If not, create the environment directory and set up a virtual environment
+    echo "Creating $DIR environment directory."
+    python3 -m venv $DIR # Virtual Environments on Python 3.5+
+fi
+
+# Activate the virtual environment
+source $DIR/bin/activate
+
+# Check if the virtual environment activation was successful
 if [[ "$VIRTUAL_ENV" == "" ]]; then
-    echo "Not able to start the  python environment"
+    echo "Not able to start the python environment."
+else 
+    echo "Activated."
 fi
